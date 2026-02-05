dac3f6d5179fdd1bbf2cdef7f2cea9e4c209e021
Jeandre Pretorius <j3andr397@gmail.com>
2026-02-05 13:03:22 +0200

    Product Sale Model added

---
+++ ProductAndSalesDisplay/Models/ProductSale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProductAndSalesDisplay.Models
+{
+    // Single product representation
+    public class ProductSale
+    {
+        public int saleId { get; set; }
+        public int productId { get; set; }
+        public double salePrice { get; set; }
+        public int saleQty { get; set; }
+        public string saleDate { get; set; }
+    }
+
+    // Collection of products
+    public class ProductSaleList
+    {
+        public List<ProductSale> Data { get; set; } = new();
+        public String Message { get; set; } = string.Empty;
+    }
+}
